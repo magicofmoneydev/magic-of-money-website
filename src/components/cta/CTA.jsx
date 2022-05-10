958f88bf20aab61ea23f74069551599c79187fa4
ernemmez <emmezeren@gmail.com>
2022-05-11 00:17:39 +0300

    cta text update

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -4,8 +4,8 @@ import './cta.css';
 const CTA = () => (
   <div className="gpt3__cta">
     <div className="gpt3__cta-content">
-      <p>Ufak Başlık</p>
-      <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elitconsequatur debitis!</h3>
+      <p>$MOM</p>
+      <h3>Join our community, don't put off your dreams. Start earning with MOM</h3>
     </div>
     <div className="gpt3__cta-btn">
        <a href="https://t.me/magicofmoney" className='buy-link' target="_blank" rel="noopener noreferrer">Join Community</a>
