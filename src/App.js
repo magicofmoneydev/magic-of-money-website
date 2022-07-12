95ce04b13a8f4f00315e8c22f42bddf9026252c1
ernemmez <emmezeren@gmail.com>
2022-07-12 16:42:26 +0300

    ss

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
-import About from './components/about/About';
-import Banner from './components/banner/Banner';
-import Brand from './components/brand/Brand';
-import Navbar from './components/Navbar/Navbar';
-import Props from './components/props/Props';
-import Roadmap from './components/roadmap/Roadmap';
-import Tokenomics from './components/tokenomics/Tokenomics';
-import CTA from './components/cta/CTA';
-import Footer from './components/footer/Footer';
-import Team from './components/team/Team';
+// import About from './components/about/About';
+// import Banner from './components/banner/Banner';
+// import Brand from './components/brand/Brand';
+// import Navbar from './components/Navbar/Navbar';
+// import Props from './components/props/Props';
+// import Roadmap from './components/roadmap/Roadmap';
+// import Tokenomics from './components/tokenomics/Tokenomics';
+// import CTA from './components/cta/CTA';
+// import Footer from './components/footer/Footer';
+// import Team from './components/team/Team';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+        <h1 style={{color:'red',textAlign:'center',marginTop:'20%'}}>
+          This website is temporarily unavailable.
+        </h1>
+      {/* <Navbar/>
       <Banner/>
       <div className='content-wrap'> 
         <Brand/>
@@ -25,7 +28,7 @@ function App() {
       </div>
       <CTA/>
       <Brand/>
-      <Footer/>
+      <Footer/> */}
     </div>
   );
 }
